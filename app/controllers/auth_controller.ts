c866349a642287f01a5e3b11a002db8d71b51744
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2024-03-13 23:17:49 -0300

    sollucionar

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -12,7 +12,10 @@ export default class AuthController {
             await userLoginValidator.validate(data)
             const user = await User.verifyCredentials(data.email, data.password)
             user.serialize()
-            const token = await User.accessTokens.create(user);
+            const token = await User.accessTokens.create(user,['*'], // with all abilities
+            {
+              expiresIn: '30 days' // expires in 30 days
+            });
 
             return response.json({success:true,results:token})
 
@@ -24,9 +27,11 @@ export default class AuthController {
     }
     async logout({response,auth} : HttpContext){
         try {
-            
-            return response.json({success:true, result: auth})
+            const user = User.find(auth.user);
+            await User.accessTokens.delete(user,1)
+            return response.json({success:true, message:'Deleted'})
         } catch (error) {
+            console.log(error)
             return response.status(error.status).json({success:false,message: 'Error de servidor'})
         }
     }
